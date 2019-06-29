2b2ae03eb5ea888a04a7e7cb91ee12c971ee7138
a982338665 <luofengli123@163.com>
2019-06-29 15:39:16 +0800

    数据回显-》input数据双向绑定

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
+//注意 AppModule 声明了应用中的所有组件，AppComponent 和 HeroesComponent。
 @NgModule({
   declarations: [
     AppComponent,
