b2a44616c3e4eaa471ead9d22aa4c3e98c17c513
a982338665 <luofengli123@163.com>
2019-06-29 16:35:32 +0800

    数据回显+路由 ： ngif/ngfor

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -11,12 +11,18 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   //添加hero属性
-  hero1 = "hero1";
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero1 = "hero1";
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
   heroes = HEROES;
+  //添加事件
+  //声明英雄对象属性Hero实际指实体类==>未初始化
+  selectedHero: Hero;
+  onSelect(hero: Hero  ): void {
+    this.selectedHero = hero;
+  }
 
   constructor() {
   }
