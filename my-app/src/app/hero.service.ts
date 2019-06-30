3a6f086848254ef953c933518c670409dae1505b
a982338665 <luofengli123@163.com>
2019-06-30 10:50:35 +0800

    业务逻辑服务化 --> 将同步获取数据修改为 异步等待获取数据

--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
+  /*getHeroes(): Hero[] {
     return HEROES;
+  }*/
+  //修改为模拟Observe数据
+  //在 HTTP 教程中，你将会调用 HttpClient.get<Hero[]>() 它也同样返回一个 Observable<Hero[]>，
+  // 它也会发出单个值，这个值就是来自 HTTP 响应体中的英雄数组
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
   constructor() { }
 }
--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -28,11 +28,22 @@ export class HeroesComponent implements OnInit {
 
   //这个参数同时做了两件事：1. 声明了一个私有 heroService 属性，2. 把它标记为一个 HeroService 的注入点。
   // 当 Angular 创建 HeroesComponent 时，依赖注入系统就会把这个 heroService 参数设置为 HeroService 的单例对象。
+  //往构造函数中添加一个私有的 heroService，其类型为 HeroService。
+  //让构造函数保持简单，只做初始化操作，比如把构造函数的参数赋值给属性。 构造函数不应该做任何事。
   constructor(private heroService: HeroService) {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    //this.heroService.getHeroes()的函数签名是同步的，此时数据是模拟数据可同步获取，当进行网络访问的时候
+    //HeroService必须要等待服务器给出响应，此处需要修改为异步函数签名
+    //它可以使用回调函数，可以返回 Promise（承诺），也可以返回 Observable（可观察对象）。
+    //这节课，HeroService.getHeroes() 将会返回 Observable，因为它最终会使用 Angular 的 HttpClient.get 方法来获取英雄数据，
+    // 而 HttpClient.get() 会返回 Observable。
+    // this.heroes = this.heroService.getHeroes();
+    //新的版本等待 Observable 发出这个英雄数组，这可能立即发生，也可能会在几分钟之后。 然后，
+    // subscribe 函数把这个英雄数组传给这个回调函数，该函数把英雄数组赋值给组件的 heroes 属性
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit() {
