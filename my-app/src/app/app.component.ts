c93fc646644d008c01759443462207c2b689efb6
a982338665 <luofengli123@163.com>
2019-09-20 17:06:09 +0800

    angular + 导出excel

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  item = [{"编号":2,"name":'李生波',"content":"{'1':'2'}"},{"编号":2,"content":"{'1':'2'}"}];
+  exportTable() {
+    const exportItem = this.item; //this.item是一个数组，包含需要导出的内容
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(exportItem);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    this.saveAsExcelFile(excelBuffer, 'report');
+  }
+
+  private saveAsExcelFile(buffer: any, fileName: string) {
+    const data: Blob = new Blob([buffer], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8'
+    });
+    FileSaver.saveAs(data, fileName + '_' + new Date().getTime() + '.xlsx');
+  }
+
 }
